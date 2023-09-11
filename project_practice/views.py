e6f022ea9fad42356496a2c460fd514e751a4984
bantud <smarty.bantu@gmail.com>
2023-09-12 02:30:15 +0530

    Django project with full stack (html animation)

---
+++ project_practice/views.py
@@ -0,0 +1,20 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def aboutUs(request):
+    
+    return HttpResponse("hello world");
+
+def htmlResponse(request):
+    data={'list':[{"name":"Rajul Dubey","age":"27","number":"9981852584"},
+                {"name":"Bantu Dubey","age":"27","number":"6232738937"},
+                {"name":"ranbir Dubey","age":"27","number":"9993507083"}]}
+
+    return render(request,'index.html',data)
+
+def dynamicRequest(request,courseId):
+    return HttpResponse(f'course id is: {courseId}')
+
+def htmlAnimation(request):
+    return render(request,"bouncingBall-fullstack.html")
